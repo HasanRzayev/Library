4909501f3e53c51b3e1e76d1aa0e26f130c85838
Rzay_ri48@itstep.edu.az <Rzay_ri48@itstep.edu.az>
2022-06-20 14:43:10 +0400

    ggggggggggggggggggggggg

--- Library/Data/AppDB.cs
+++ Library/Data/AppDB.cs
@@ -21,7 +21,7 @@ namespace Library.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("   Data Source = WIN-EA8010O87DM;Initial Catalog=Library;Integrated Security = True; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("   Data Source = STHQ0125-06;Initial Catalog=Library;User ID = admin; Password=admin; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
